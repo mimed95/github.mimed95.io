2afe8012403bedc8c2e6c5f37b8a3d9169e9e100
Michael Medvedovski <52825236+mimed95@users.noreply.github.com>
2023-06-26 01:05:48 +0200

    github.io config added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-//    site: 'https://mimed95.gitub.io'
+   site: 'https://mimed95.gitub.io'
 });
